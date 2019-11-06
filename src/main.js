78c61c3e68b91dd3450f5403afdea11c65533819
afraniocosta <afraniompc@gmail>
2019-11-05 22:13:40 -0300

    aula async await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-alert("Opa");
+// promise comum. Uma forma de fazer com que uma coisa demore para executar. É um delay, um asincronismo.
+ 
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// minhaPromise().then(response=>{
+//     console.log(response)
+// })
+// .catch(err => {
+
+// });
+
+// abaixo segue a sintaxe do asyc await
+
+async function executaPromise(){
+    const response = await minhaPromise();
+    console.log(response);
+}
+
+executaPromise();
