f03701740ecb044f805d028c7aa4a4562c063b8e
afraniocosta <afraniompc@gmail>
2019-11-05 22:32:47 -0300

    configurando axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username){
+        try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response);
+        } catch (err) {
+            console.warn('Erro na API');
+        }
+    }
+}
+
+Api.getUserInfo('diego3d');
+
+
 // promise comum. Uma forma de fazer com que uma coisa demore para executar. É um delay, um asincronismo.
  
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('OK') }, 2000);
-});
+// const minhaPromise = () => new Promise((resolve, reject) => {
+//     setTimeout(() => { resolve('OK') }, 2000);
+// });
 
 // minhaPromise().then(response=>{
 //     console.log(response)
@@ -13,9 +29,9 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 // abaixo segue a sintaxe do asyc await
 
-async function executaPromise(){
-    const response = await minhaPromise();
-    console.log(response);
-}
+// async function executaPromise(){
+//     const response = await minhaPromise();
+//     console.log(response);
+// }
 
-executaPromise();
+// executaPromise();
