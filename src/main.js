95b4da1805beca79444751e4625e792e50d056f4
afraniocosta <afraniompc@gmail>
2019-11-20 21:37:50 -0300

    novos arquivos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from './api'; 
+
 class App {
   constructor(){
       this.repositories = [];
 
       this.formEl = document.getElementById('repo-form');
+      this.inputEl = document.querySelector('input[name=repository]')
       this.listEl = document.getElementById('repo-list')
       this.registerHandlers();
 
@@ -12,7 +15,7 @@ class App {
       this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
       event.preventDefault();
 
       this.repositories.push({
@@ -29,6 +32,28 @@ class App {
 
   render(){
     this.listEl.innerHTML = '';
+    this.repositories.forEach(repo=>{
+
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'))
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl)
+
+    });
 
   }
 
